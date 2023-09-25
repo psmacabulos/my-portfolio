3ec5c2b782fca5dc3be4501911d154c60f3d0302
psmacabulos <107623965+psmacabulos@users.noreply.github.com>
2023-09-25 18:05:34 +0800

    Changed Hero description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,9 +14,9 @@ const Home = () => {
           I&apos;m a Full Stack Developer.
         </h2>
         <p className='py-4 max-w-[700px] text-[#8892b0]'>
-          As a skilled web developer, I specialize in crafting modern solutions that solve complex
-          business problems. With a strong focus on collaboration and independence, I thrive in both
-          team-based and self-directed projects.
+          I specialize in crafting modern solutions that solve complex business problems. With a
+          strong focus on collaboration and independence, I thrive in both team-based and
+          self-directed projects.
         </p>
         <div className='mt-2'>
           <button className='group text-white border-2 px-6 py-3 flex items-center hover:bg-[#2ebfa5] hover:border-[#2ebfa5] duration-700'>
